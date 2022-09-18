780bafe8d67da98aa80a5fe17cf6894f8ad1f03b
tpleehan <tpleehan@gmail.com>
2022-09-18 21:55:59 +0900

    react-study 리액트를 다루는 기술 리덕스를 사용하여 리액트 애플리케이션 상태 관리하기

---
+++ react-redux-tutorial/src/containers/TodosContainer.js
@@ -0,0 +1,30 @@
+import React, {useCallback} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {changeInput, insert, remove, toggle} from "../modules/todos";
+import Todos from "../components/Todos";
+import useActions from "../lib/useActions";
+
+const TodosContainer = () => {
+  const {input, todos} = useSelector(({todos}) => ({
+    input: todos.input,
+    todos: todos.todos
+  }));
+
+  const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+    [changeInput, insert, toggle, remove],
+    []
+  );
+
+  return (
+    <Todos
+      input={input}
+      todos={todos}
+      onChangeInput={onChangeInput}
+      onInsert={onInsert}
+      onToggle={onToggle}
+      onRemove={onRemove}
+    />
+  );
+};
+
+export default TodosContainer;
